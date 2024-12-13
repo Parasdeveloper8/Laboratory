60670745ad4befcdde1c1c7ef9fa248fcb09983b
Paras prajapat <parasprajapat8th.gmail.com>
2024-12-13 14:17:33 +0530

    Added file to database completed

--- Routes/POST-routes/handlepost.go
+++ Routes/POST-routes/handlepost.go
@@ -1,23 +1,54 @@
 package postroutes
 
 import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func HandlePost(c *gin.Context) {
-	//single file upload
+	/*configs, err := reusable_structs.Init()
+	if err != nil {
+		fmt.Println("Failed to load configurations", err)
+	}*/
+	db, err := sql.Open("mysql", "root:8890Aaaa@@tcp(localhost:3306)/laboratory")
+	if err != nil {
+		log.Printf("Failed to connect to database: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	defer db.Close()
+	// Parse the file from the request
 	file, err := c.FormFile("file")
+	/*caption := c.PostForm("caption")*/
 	if err != nil {
-		c.String(http.StatusBadRequest, "Failed to upload file: %s", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to get file"})
 		return
 	}
-	//save file to server
-	err = c.SaveUploadedFile(file, "./Uploads/"+file.Filename)
+	// Read file content
+	fileContent, err := file.Open()
 	if err != nil {
-		c.String(http.StatusInternalServerError, "Unable to save file: %s", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read file content"})
 		return
 	}
-	c.String(http.StatusOK, "File uploaded successfully: %s", file.Filename)
+	defer fileContent.Close()
+	fileBytes, err := io.ReadAll(fileContent)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read file content"})
+		return
+	}
+	//insert file into DB
+	query := "insert into laboratory.posts(name,content,email) values(?,?,?)"
+	_, err = db.Exec(query, file.Filename, fileBytes, "ppdev@gmail.com")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save file to database"})
+		fmt.Println(err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "File uploaded successfully", "filename": file.Filename})
 }
--- Structs/Struct.go
+++ Structs/Struct.go
@@ -1,8 +1,38 @@
 package reusable_structs
 
-//element struct
+import (
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+// element struct
 type Element struct {
 	Name        string  `json:"name"`
 	Atomic_Mass float64 `json:"atomic_mass"`
 	Symbol      string  `json:"symbol"`
+} //configurations struct
+type Configurations struct {
+	DB_URL              string `envconfig:"DB_URL"` //Binds to DB_URL in env
+	SECRETKEY           string `envconfig:"SECRETKEY"`
+	DB_HOST             string `envconfig:"DB_HOST"`
+	DB_USER             string `envconfig:"DB_USER"`
+	DB_PASSWORD         string `envconfig:"DB_PASSWORD"`
+	DB_NAME             string `envconfig:"DB_NAME"`
+	DB_CONNECTION_LIMIT int    `envconfig:"DB_CONNECTION_LIMIT"`
+	EMAIL               string `envconfig:"EMAIL"`
+	EMAIL_PASSWORD      string `envconfig:"EMAIL_PASSWORD"`
+}
+
+var GlobalConfigurations *Configurations
+
+func Init() (*Configurations, error) {
+	var cfg Configurations
+	// Load environment variables into the struct
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	GlobalConfigurations = &cfg
+	return &cfg, nil
 }
