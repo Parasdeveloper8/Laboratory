e9d0a0b54dfbb89c80e28175fb70cc6a93b30a18
Paras prajapat <parasprajapat8th.gmail.com>
2025-01-27 19:53:10 +0530

    Delete-routes ' files code shortened

--- Routes/DELETE-routes/handlePostDelete.go
+++ Routes/DELETE-routes/handlePostDelete.go
@@ -1,10 +1,8 @@
 package deleteroutes
 
 import (
-	reusable_structs "Laboratory/Structs"
-	"database/sql"
+	reusable "Laboratory/Reusable"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,21 +16,11 @@ func HandleDeletePost(c *gin.Context) {
 	post_id := c.Param("post_id")
 
 	// Delete post from database
-	configs, err := reusable_structs.Init()
-	if err != nil {
-		log.Printf("Failed to load configurations: %v", err)
-	}
-
-	db, err := sql.Open("mysql", configs.DB_URL)
-	if err != nil {
-		log.Printf("Failed to connect to database: %v", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-		return
-	}
+	db := reusable.LoadSQLStructConfigs(c)
 	defer db.Close()
 	//delete all data of post
 	query := "DELETE FROM laboratory.posts WHERE post_id = ?"
-	_, err = db.Exec(query, post_id)
+	_, err := db.Exec(query, post_id)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		fmt.Println("Failed to delete post from database")
