fbffa259ccffe243626b1aebe6031c175086fc60
Paras prajapat <parasprajapat8th.gmail.com>
2025-01-28 17:02:42 +0530

    [update] : Functionality to stop like if user has already liked post implemented

--- Routes/POST-routes/handleLikes.go
+++ Routes/POST-routes/handleLikes.go
@@ -2,6 +2,7 @@ package postroutes
 
 import (
 	reusable "Laboratory/Reusable"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -23,9 +24,35 @@ func HandleLikes(c *gin.Context) {
 	//get answer id from path parameters
 	ans_id := c.Param("ansId")
 
+	//lets check if like by sessionEmail in database is already present
+	query := "select likes.ans_id , likes.email from laboratory.likes where ans_id = ?"
+	rows, err := db.Query(query, ans_id)
+	if err != nil {
+		fmt.Println("Failed to query row", err)
+	}
+
+	//let's bind returned rows
+	type returnedRows struct {
+		Email string `db:"email"`
+		AnsId string `db:"ans_id"`
+	}
+	for rows.Next() {
+		var r returnedRows
+		err := rows.Scan(&r.AnsId, &r.Email)
+		if err != nil {
+			log.Printf("Failed to scan row: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		if r.Email == sessionEmail {
+			log.Println("You have already liked post")
+			c.JSON(http.StatusBadRequest, gin.H{"info": "You have already liked post"})
+			return //Stop function executing further
+		}
+	}
 	//query
-	query := "insert into laboratory.likes(email,ans_id) values(?,?)"
-	_, err := db.Exec(query, sessionEmail, ans_id)
+	query = "insert into laboratory.likes(email,ans_id) values(?,?)"
+	_, err = db.Exec(query, sessionEmail, ans_id)
 	if err != nil {
 		log.Printf("Error on sending email and ansId %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
