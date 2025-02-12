9aa0113ee1d3c90e7f41cf681ee5fd68c144eca8
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-02-12 18:47:29 +0530

    [update]:implemented pipelines in getSym&Val.go

--- GETAPI/getSym&Val.go
+++ GETAPI/getSym&Val.go
@@ -1,39 +1,45 @@
 package GETAPI
 
 import (
+	reusable "Laboratory/Reusable"
 	reusable_structs "Laboratory/Structs"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
+
+//pipelines ahead
 
 /*
 Function to send elements2.json to frontend for creating chemical formulaes.
 */
 func GetSymbolValency(c *gin.Context) {
-	// Open the elements2 file
-	file, err := os.Open("D:/laboratory/JSON/elements2.json") // Path to elements.json
-	if err != nil {
-		fmt.Printf("Could not open file: %v\n", err) //Could not open file
-
-	}
+	//Stage I
+	file := reusable.OpenJSONFile("D:/laboratory/JSON/elements2.json")
 	defer file.Close()
 
-	// Read file contents
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Printf("Could not read file: %v\n", err) //Could not read file contents
+	//Stage II
+	bytes := reusable.ReadJSONfileByte(file)
 
-	}
+	//channel
+	elemVal := make(chan []reusable_structs.ElementsValAnCation)
 
-	// Parse JSON into slice of elements
-	var elemWithValency []reusable_structs.ElementsValAnCation // struct in Structs/Struct4.go
-	if err := json.Unmarshal(bytes, &elemWithValency); err != nil {
-		log.Fatalf("Failed to parse JSON: %v", err) //parse to json failed
-	}
-	c.JSON(200, gin.H{"data": elemWithValency})
+	go func() {
+		//Stage III
+		// Parse JSON into slice of elements
+		var elemWithValency []reusable_structs.ElementsValAnCation // struct in Structs/Struct4.go
+		if err := json.Unmarshal(bytes, &elemWithValency); err != nil {
+			log.Fatalf("Failed to parse JSON: %v", err) //parse to json failed
+		}
+		//sender
+		elemVal <- elemWithValency
+
+		close(elemVal)
+
+	}()
+	//receiver
+	elemValData := <-elemVal
+
+	c.JSON(200, gin.H{"data": elemValData})
 }
