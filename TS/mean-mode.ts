aae5281abec632ad35202de036f92d5dde861f6b
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-03-01 21:23:31 +0530

    [update]:Added a script file

---
+++ TS/mean-mode.ts
@@ -0,0 +1,7 @@
+//Ids from meMoMedPage.html
+const form : HTMLElement | null = document.getElementById("myform");
+
+//stop form reloading on submit
+//form?.addEventListener("submit",(event)=>event.preventDefault());
+
+export{}
